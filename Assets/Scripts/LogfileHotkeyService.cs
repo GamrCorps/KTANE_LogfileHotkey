10d02a6b690104133caa84c351cf5150b8d29fc3
Matthew McCaskill <GamrCorps@gmail.com>
2018-04-11 15:34:22 -0700

    Initial Commit

---
+++ Assets/Scripts/LogfileHotkeyService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using UnityEngine;
+
+using Newtonsoft.Json;
+
+public class LogfileHotkeyService : MonoBehaviour {
+
+    public KMModSettings ModSettings;
+
+    class Settings {
+        public string keyViewLogfile;
+        public string keyClearLogfile;
+    }
+    Settings keySettings;
+
+    KeyCode viewCode;
+    bool[] viewModifiers; // CTRL ALT WINDOWS SHIFT
+    KeyCode clearCode;
+    bool[] clearModifiers;
+
+    public void Start() {
+        string settings = string.Join("\n", ModSettings.Settings.Split('\n').Where(x => !x.Trim().StartsWith("//")).ToArray());
+        keySettings = JsonConvert.DeserializeObject<Settings>(settings);
+        SetupKey(out viewCode, out viewModifiers, keySettings.keyViewLogfile, KeyCode.F8, new bool[4] { false, false, false, true });
+        SetupKey(out clearCode, out clearModifiers, keySettings.keyClearLogfile, KeyCode.F9, new bool[4] { false, false, false, true });
+        Debug.LogFormat("[LogfileHotkey] \"View Log\" key combination set to: \"{0}{1}{2}{3}{4}\".", viewModifiers[0] ? "CTRL+" : "", viewModifiers[1] ? "ALT+" : "", viewModifiers[2] ? "WIN/CMD+" : "", viewModifiers[3] ? "SHIFT+" : "", viewCode);
+        Debug.LogFormat("[LogfileHotkey] \"Clear Log\" key combination set to: \"{0}{1}{2}{3}{4}\".", clearModifiers[0] ? "CTRL+" : "", clearModifiers[1] ? "ALT+" : "", clearModifiers[2] ? "WIN/CMD+" : "", clearModifiers[3] ? "SHIFT+" : "", clearCode);
+    }
+
+    public void Update() {
+        if (Input.GetKeyDown(viewCode) && CheckModifiers(ref viewModifiers)) {
+            LogfileUploader.Instance.Open();
+        }
+        if (Input.GetKeyDown(clearCode) && CheckModifiers(ref clearModifiers)) {
+            LogfileUploader.Instance.Clear();
+            Debug.LogFormat("[LogfileHotkey] Cleared internal log file.");
+        }
+    }
+
+    private void SetupKey(out KeyCode keyCode, out bool[] modifiers, string keyCombination, KeyCode defaultKeyCode, bool[] defaultKeyModifiers) {
+        if (Regex.IsMatch(keyCombination, @"^[\^!@_]*([a-zA-Z0-9]+)$")) {
+            modifiers = new bool[4] {
+                keyCombination.Contains("^"),
+                keyCombination.Contains("!"),
+                keyCombination.Contains("@"),
+                keyCombination.Contains("_"),
+            };
+            try {
+                keyCode = (KeyCode)Enum.Parse(typeof(KeyCode), Regex.Match(keyCombination, @"^[\^!@_]*([a-zA-Z0-9]+)$").Groups[1].ToString());
+            } catch (ArgumentException) {
+                Debug.LogFormat("[LogfileHotkey] Invalid KeyCode in key combination: \"{0}\".", keyCombination);
+                modifiers = defaultKeyModifiers;
+                keyCode = defaultKeyCode;
+            }
+        } else {
+            Debug.LogFormat("[LogfileHotkey] Invalid key combination in settings: \"{1}\".", keyCombination);
+            modifiers = defaultKeyModifiers;
+            keyCode = defaultKeyCode;
+        }
+    }
+
+    private bool CheckModifiers(ref bool[] modifiers) {
+        return (modifiers[0] ? Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) : true) &&
+               (modifiers[1] ? Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt) || Input.GetKey(KeyCode.AltGr) : true) &&
+               (modifiers[2] ? Input.GetKey(KeyCode.LeftWindows) || Input.GetKey(KeyCode.RightWindows) || Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.RightCommand) || Input.GetKey(KeyCode.LeftApple) || Input.GetKey(KeyCode.RightApple) : true) &&
+               (modifiers[3] ? Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) : true);
+    }
+}
---
+++ Assets/Scripts/LogfileUploader.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using UnityEngine;
+
+// Shamelessly borrowed and modified from Twitch Plays KTANE code.
+// Original file by CaitSith2, red031000, and samfun123
+
+public class LogfileUploader : MonoBehaviour {
+    public static LogfileUploader Instance;
+
+    public string Log { get; private set; }
+
+    [HideInInspector]
+    public string analysisUrl = null;
+
+    [HideInInspector]
+    public string LOGPREFIX = "[LogfileHotkey] ";
+
+    private string[] _blacklistedLogLines = {
+       "[PaceMaker]",
+       "[ServicesSteam]",
+       "[BombGenerator] Instantiated EmptyComponent",
+       "[BombGenerator] Filling remaining spaces with empty components.",
+       "[BombGenerator] BombTypeEnum: Default",
+       "[StatsManager]",
+       "[FileUtilityHelper]",
+       "[MenuPage]",
+       "[PlayerSettingsManager]",
+       "[LeaderboardBulkSubmissionWorker]",
+       "[MissionManager]",
+       "[AlarmClock]",
+       "[AlarmClockExtender]",
+       "[Alarm Clock Extender]",
+       "[BombGenerator] Instantiated TimerComponent",
+       "[BombGenerator] Instantiating RequiresTimerVisibility components on",
+       "[BombGenerator] Instantiating remaining components on any valid face.",
+       "[PrefabOverride]",
+       "Tick delay:",
+       "Calculated FPS: ",
+       "[ModuleCameras]",
+       "[TwitchPlays]",
+       "(Filename:  Line: 21)"
+    };
+
+    private OrderedDictionary domainNames = new OrderedDictionary {
+		// In order of preference (favourite first)
+		// The integer value is the data size limit in bytes
+		{ "hastebin.com", 400000 },
+        { "ktane.w00ty.com", 2000000 }
+    };
+
+    public void Awake() {
+        Instance = this;
+    }
+
+    public void OnEnable() {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    public void OnDisable() {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    public void Clear() {
+        Log = "";
+    }
+
+    public string Flush() {
+        string result = Log;
+        Log = "";
+        return result;
+    }
+
+    public void Open(bool openLink = true) {
+        analysisUrl = null;
+        StartCoroutine(DoPost(Log, openLink));
+    }
+
+    private IEnumerator DoPost(string data, bool openLink) {
+        // This first line is necessary as the Log Analyser uses it as an identifier
+        data = "Initialize engine version: Twitch Plays\n" + data;
+
+        byte[] encodedData = System.Text.Encoding.UTF8.GetBytes(data);
+        int dataLength = encodedData.Length;
+
+        bool tooLong = false;
+
+        foreach (DictionaryEntry domain in domainNames) {
+            string domainName = (string)domain.Key;
+            int maxLength = (int)domain.Value;
+
+            tooLong = false;
+            if (dataLength >= maxLength) {
+                Debug.LogFormat(LOGPREFIX + "Data ({0}B) is too long for {1} ({2}B)", dataLength, domainName, maxLength);
+                tooLong = true;
+                continue;
+            }
+
+            Debug.Log(LOGPREFIX + "Posting new log to " + domainName);
+
+            string url = "https://" + domainName + "/documents";
+
+            WWW www = new WWW(url, encodedData);
+
+            yield return www;
+
+            if (www.error == null) {
+                // example result
+                // {"key":"oxekofidik"}
+
+                string key = www.text;
+                key = key.Substring(0, key.Length - 2);
+                key = key.Substring(key.LastIndexOf("\"") + 1);
+                string rawUrl = "https://" + domainName + "/raw/" + key;
+
+                Debug.Log(LOGPREFIX + "Paste now available at " + rawUrl);
+
+                analysisUrl = LogAnalyserFor(rawUrl);
+
+                if (openLink) {
+                    OpenLink();
+                }
+
+                break;
+            } else {
+                Debug.Log(LOGPREFIX + "Error: " + www.error);
+            }
+        }
+
+        if (tooLong) {
+            Debug.Log(LOGPREFIX + "Error uploading log file.");
+        }
+
+        yield break;
+    }
+
+    public bool OpenLink() {
+        if (string.IsNullOrEmpty(analysisUrl)) {
+            Debug.Log(LOGPREFIX + "No analysis URL available, can't open.");
+            return false;
+        }
+        Debug.Log(LOGPREFIX + "Opening link in default browser.");
+        Application.OpenURL(analysisUrl);
+        return true;
+    }
+
+    private void HandleLog(string message, string stackTrace, LogType type) {
+        if (_blacklistedLogLines.Any(message.StartsWith)) return;
+        if (message.StartsWith("Function ") && message.Contains(" may only be called from main thread!")) return;
+        Log += message + "\n";
+    }
+
+    public string LogAnalyser = "https://ktane.timwi.de/More/Logfile%20Analyzer.html";
+
+    public string LogAnalyserFor(string url) {
+        return string.Format(LogAnalyser + "#url={0}", url);
+    }
+}
